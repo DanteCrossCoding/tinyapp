9dce847c33207aae04f73c5b8be1a4231d7e593d
Dante Cross <dantecross@outlook.com>
2020-11-10 01:11:13 +0000

    added POST function and number generator for url

--- express_server.js
+++ express_server.js
@@ -4,20 +4,37 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+function generateRandomString(length = 6) {
+  return Math.random().toString(20).substr(2, length);
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send(generateRandomString());
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase};
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
