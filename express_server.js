548c9c92794481ff8e71091c572eb6e53eb2e048
Dante Cross <dantecross@outlook.com>
2020-11-11 21:37:29 +0000

    added username display on login and logout features

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080;
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -38,16 +40,24 @@ app.get("/u/:shortURL", (req, res) => {
 })
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { 
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL]};
   let longURL = urlDatabase[req.params.shortURL];
   res.render("urls_show", templateVars);
 });
@@ -76,6 +86,16 @@ app.post("/urls", (req, res) => {
   urlDatabase[shortURL] = longURL
   res.redirect("/urls");
 });
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username", req.body)
+  res.redirect("/urls");
+})
 
 
 app.listen(PORT, () => {
